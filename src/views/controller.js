bb25f878be06872da2cd906fecf30ee805b59114
J. G. Manzano <javier.garcia@s4g.es>
2017-11-26 17:00:51 +0100

    Started defining Angular functions.

--- src/views/controller.js
+++ src/views/controller.js
@@ -1,4 +1,29 @@
+
 angular.module('Berry-move', [])
 .controller('BerryController', function($scope, $http, $window, AuthenticationService) {
     const controller = this;
+
+    controller.originOrg = {};
+    controller.destinationOrg = {};
+    controller.stagedMetadata = {}; // Represents the selected metadata tu deploy.
+
+    // Logins to an org.
+    controller.login = function(username, password, org) {
+
+    }
+
+    // Retrieves metadata from origin org..
+    controller.retrieveMetadata = function() {
+
+    }
+
+    // Deploys staged metadata to destination org..
+    controller.deployMetadata = function() {
+        
+    }
+
+    // Adds metadata from the display list to the stage.
+    controller.stageMetadata = function(metadata){
+
+    }
 });
