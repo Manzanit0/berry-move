165fc6f3e46ab9400bef260b77db0e7bef12810a
J. G. Manzano <javier.garcia@s4g.es>
2017-12-10 21:01:45 +0100

    Removed the code that isn't involved in the ch1 article.

--- src/endpoints/routes.js
+++ src/endpoints/routes.js
@@ -3,11 +3,9 @@
 const app = require('express');
 
 const metadata = require('./metadata.endpoint.js');
-const organization = require('./organization.endpoint');
 
 const routes = (app) => {
     app.use('/metadata', metadata);
-    app.use('/org', organization);
 };
 
 module.exports = routes;
